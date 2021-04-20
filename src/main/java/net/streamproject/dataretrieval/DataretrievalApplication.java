bb4374e91aa7b1228ba27117fb88d9781e77a40c
TBoonX <tboonx@googlemail.com>
2021-04-20 14:10:51 +0200

    Try to really enable cors
    Does not work

--- src/main/java/net/streamproject/dataretrieval/DataretrievalApplication.java
+++ src/main/java/net/streamproject/dataretrieval/DataretrievalApplication.java
@@ -2,12 +2,23 @@ package net.streamproject.dataretrieval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DataretrievalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataretrievalApplication.class, args);
+	}
+	
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080").allowedOrigins("http://localhost");
+			}
+		};
 	}
 
 }
