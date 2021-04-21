0972b276a3fa93d77275da1a047e0fdebcef209d
TBoonX <tboonx@googlemail.com>
2021-04-21 09:45:46 +0200

    Remove CORS configuration because Spring stops working

--- src/main/java/net/streamproject/dataretrieval/DataretrievalApplication.java
+++ src/main/java/net/streamproject/dataretrieval/DataretrievalApplication.java
@@ -2,43 +2,12 @@ package net.streamproject.dataretrieval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DataretrievalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataretrievalApplication.class, args);
-	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://stream-dataspace.net").allowedOrigins("http://localhost");
-			}
-		};
-	}
-	
-	@Bean
-	CorsWebFilter corsFilter() {
-	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.addAllowedOrigin("http://localhost");
-	    config.addAllowedOrigin("https://stream-dataspace.net");
-	    config.addAllowedHeader("*");
-	    config.addAllowedMethod("*");
-
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", config);
-
-	    return new CorsWebFilter(source);
 	}
 
 }
